91c2611dcdefd9710b0373e1aa9ec906c8623118
NayllaMiranda <135022291+NayllaMiranda@users.noreply.github.com>
2024-03-25 11:44:38 -0300

    parte 2 25/03

--- app/usuario/[id]/page.tsx
+++ app/usuario/[id]/page.tsx
@@ -1,11 +1,18 @@
 import Link from 'next/link';
 
+import usuarios from '@/src/usuario';
+
 export default function PerfilUsuario( {params}: {params: {id: string } }) {
+
+    const Usuário = usuarios.filter((usu, index, Array) => usu.id === parseInt(params.id));
     return (
         <>
             <h1>Perfil</h1>
-           
-         <p>Nome do usuario {params.id}</p>
+            <h1>
+                <p>Nome: {Usuário[0].nome}</p>
+                <p>Altura: {Usuário[0].altura}</p>
+                <p>Ativo: {Usuário[0].ativo ? 'sim' : 'não'}</p>
+            </h1>
            
 
             <p><Link href={'/'}>Voltar</Link></p>
